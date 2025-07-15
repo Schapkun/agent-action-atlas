a6a52ad9c8ed3da72bede91a505ba8432608dc7d
Schapkun <info@schapkun.com>
2025-07-15 16:21:22 +0200

    Update next.config.js

--- preview_version/next.config.js
+++ preview_version/next.config.js
@@ -1,19 +1,14 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // LET OP: Zorg dat je NIET per ongeluk 'next export' draait.
-  // We willen een server-build zodat /api routes werken.
-  // (Standaard is output: 'server', dus hier niets extra’s nodig.)
-
-  // Als je wél wilt dat Next.js ook de map 'src/pages' herkent, staat dat standaard aan:
-  // je hoeft pageExtensions of zo niet aan te passen.
-
-  // Mochten we ooit aliases willen gebruiken:
-  // webpack(config) {
-  //   config.resolve.alias['@preview'] = path.resolve(__dirname, 'preview_version/src/pages/api')
-  //   return config
-  // }
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias['/preview_version'] = path.join(__dirname, 'preview_version');
+    }
+    return config;
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
