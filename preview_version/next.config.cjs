af7049c1a3ed02eba7a6cef0326ab7c377e8e146
Schapkun <info@schapkun.com>
2025-07-24 16:16:04 +0200

    Update next.config.cjs

--- preview_version/next.config.cjs
+++ preview_version/next.config.cjs
@@ -3,7 +3,6 @@ const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname, "src");
     config.resolve.fallback = {
